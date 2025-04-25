52808f995b9b4d72c085d7a8b38cdae252b65c16
Nolann Parcheminer <96395344+Nolanndev@users.noreply.github.com>
2025-04-25 13:09:19 +0200

    test4

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>👋🏻 Hello Application !!</h1>');
+    res.send('<h1>👋🏻 Hello Application !!</h1><p>Un début de paragraphe</p>');
 })
 
 const INTERNAL_PORT = process.env.PORT || 3000;
