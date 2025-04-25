09bb3a943e75ea008a387b7a3431f9f9490bc647
Nolann Parcheminer <96395344+Nolanndev@users.noreply.github.com>
2025-04-25 22:46:20 +0200

    test6

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>👋🏻 Hello Application !!</h1><p>Un début de §paragraphe§</p>');
+    res.send(`
+        <h1>👋🏻 Hello Application !!</h1>
+        <p>Un début de §paragraphe§</p>
+        <p>✅ Ça à l'air de fonctionner</p>
+    `);
 })
 
 const INTERNAL_PORT = process.env.PORT || 3000;
 const EXTERNAL_PORT = process.env.EXTERNAL_PORT || INTERNAL_PORT;
 
 app.listen(INTERNAL_PORT, () => {
-    console.log(`INTERNAL_PORT: ${INTERNAL_PORT}`);
-    console.log(`EXTERNAL_PORT: ${EXTERNAL_PORT}`);
     console.log(`Server is running on http://localhost:${EXTERNAL_PORT}`);
 });
